8dfd3ea58ff916bb33a56abb6fa763e61ffb6e3a
= <=>
2015-10-06 14:14:36 -0700

    Initial code checkin.

---
+++ GameDice/Window1.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameDice
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        Random _random = new Random();
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRoll_Click(object sender, RoutedEventArgs e)
+        {
+            int dice = 0;
+            int sides = 0;
+            Int32.TryParse(txtNumDice.Text, out dice);
+            Int32.TryParse(txtNumSides.Text, out sides);
+            if (dice > 0 && sides > 0)
+            {
+                int total = 0;
+                txtResult.Text = String.Empty;
+                for (int i = 0; i < dice; i++)
+                {
+                    int val = _random.Next(1, sides+1);
+                    total += val;
+                    if (i > 0)
+                        txtResult.Text += ", ";
+                    txtResult.Text += val.ToString(); 
+                }
+                txtTotal.Text = total.ToString();
+            }
+        }
+
+        private void btnAbout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ZC Dice is Copyright (c) 2010 Zeta Centauri, Inc.\nZC Dice is freeware and may be distributed freely.\nWritten by Jason Champion.\nhttp://zetacentauri.com");
+        }
+    }
+}
